e72a8c49da8e15816b7e4a3e089c727eed554372
Benjamin Howes <howesben@gmail.com>
2013-11-10 00:58:04 -0500

    Added title and Accordion for sliders

---
+++ scripts/bh_jqui.js
@@ -0,0 +1,11 @@
+
+
+
+//get the document ready
+$(document).ready(function() {
+	
+	
+	//1 - enable accordians
+    $('.accord').accordion({ collapsible: true, active: false });
+    $('#dragMe').draggable();
+});
