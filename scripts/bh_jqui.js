bb369a32f2aa1906140a90dcabee01ca39fcdbbf
jschumacher <jschumacher@thorntontomasetti.com>
2013-11-10 15:03:28 -0500

    Sliders working.  Globals are being populated

--- scripts/bh_jqui.js
+++ scripts/bh_jqui.js
@@ -4,9 +4,9 @@ var maxLength = -1.0;
 var minDisp = -1.0;
 var maxDisp = -1.0;
 
-var lengthSliderMin = -1.0;
+var lengthSliderMin = 1000000;
 var lengthSliderMax = -1.0;
-var dispSliderMin = -1.0;
+var dispSliderMin = 1000000;
 var dispSliderMax = -1.0;
 
 
@@ -19,11 +19,17 @@ $(document).ready(function () {
 
         //get current curve length
         var len = myCurvesArray[i]["Length"];
-        console.log(len);
+        var L = parseFloat(len);
 
         //if it's greater than our max, it's the new max
+        if (L > lengthSliderMax) {
+            lengthSliderMax = L;
+        };
 
         //if it's less than our min, it's the new min
+        if (L < lengthSliderMin) {
+            lengthSliderMin = L;
+        };
     };
 
     //0 - TO DO - populate min and max values of sliders using geometry data
@@ -31,11 +37,18 @@ $(document).ready(function () {
 
         //get current curve length
         var deflection = myVerticesArray[i]["Deflection"];
-        console.log('def: ' + deflection);
+        var D = parseFloat(deflection);
+        D = D / 100000000000;
 
         //if it's greater than our max, it's the new max
+        if (D > dispSliderMax) {
+            dispSliderMax = D;
+        };
 
         //if it's less than our min, it's the new min
+        if (D < dispSliderMin) {
+            dispSliderMin = D;
+        };
     };
 
     //1 - enable accordians
@@ -43,47 +56,66 @@ $(document).ready(function () {
     $('#dragMe').draggable();
 
     //2 - set up sliders
+
+    //length slider
     $('#lengthSlider').slider({
         range: true,
-        min: 10.00,
-        max: 40.00,
+        min: lengthSliderMin,
+        max: lengthSliderMax,
         step: 0.01,
-        values: [12, 35],     //these should be our globals after they are populated
+        values: [lengthSliderMin, lengthSliderMax],     //these should be our globals after they are populated
+        //on slide event:
         slide: function (event, ui) {
-            $('#length').val(ui.values[0] + " - " + ui.values[1] + "m");
-            minLength = ui.values[0];
-            maxLength = ui.values[1];
+
+            var myMin = ui.values[0];
+            var myMax = ui.values[1];
+            minLength = myMin.toFixed(3);
+            maxLength = myMax.toFixed(3);
+            $('#length').val(minLength + " to " + maxLength + "m");
 
             //debugging - log values;
-            console.log("minLength = " + minLength);
-            console.log("maxLength = " + maxLength);
+            //console.log("minLength = " + minLength);
+            //console.log("maxLength = " + maxLength);
         }
     });
+
+    //set values first time through
+    var minL = $('#lengthSlider').slider("values", 0);
+    var maxL = $('#lengthSlider').slider("values", 1);
     $('#length').val(
-        $('#lengthSlider').slider("values", 0)
-        + " - " +
-        $('#lengthSlider').slider("values", 1) + "m"
+        minL.toFixed(3)
+        + " to " +
+        maxL.toFixed(3) + "m"
     );
 
+
+
+    //disp slider
     $('#dispSlider').slider({
         range: true,
-        min: 0.00,
-        max: 10.00,
+        min: dispSliderMin,
+        max: dispSliderMax,
         step: 0.001,
-        values: [0, 10],
+        values: [dispSliderMin, dispSliderMax],
         slide: function (event, ui) {
-            $('#disp').val(ui.values[0] + " - " + ui.values[1] + "m");
-            minDisp = ui.values[0];
-            maxDisp = ui.values[1];
+            var myMin = ui.values[0];
+            var myMax = ui.values[1];
+            minDisp = myMin.toFixed(3);
+            maxDisp = myMax.toFixed(3);
+            $('#disp').val(minDisp + " to " + maxDisp + "m");
 
             //debugging - log values;
-            console.log("minDisp = " + minDisp);
-            console.log("maxDisp = " + maxDisp);
+            //console.log("minDisp = " + minDisp);
+            //console.log("maxDisp = " + maxDisp);
         }
     });
+
+    //set values first time through
+    var minD = $('#dispSlider').slider("values", 0);
+    var maxD = $('#dispSlider').slider("values", 1);
     $('#disp').val(
-        $('#dispSlider').slider("values", 0)
-        + " - " +
-        $('#dispSlider').slider("values", 1) + "m"
+        minD.toFixed(3)
+        + " to " +
+        maxD.toFixed(3) + "m"
     );
 });
